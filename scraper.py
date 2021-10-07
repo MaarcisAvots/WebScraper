c7471c0bfef7eb2c56398cb78cdf8399fbfe270a
ReinisZonne <reiniszonne@inbox.lv>
2021-10-07 11:57:45 +0300

    show info in console

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
 import time
+from bs4 import BeautifulSoup as bs
+import csv
 
 URL = "http://books.toscrape.com/catalogue"
 LAPAS = "lapas/"
@@ -19,4 +21,34 @@ def lejupieladet_lapas(cik):
         saglabat(f"{URL}/page-{i}.html", f"{i}_lapa.html")
         time.sleep(1)
 
-lejupieladet_lapas(5)
+def info(datne):
+    dati = []
+    with open(datne, 'r', encoding='UTF-8') as f:
+        html = f.read()
+
+    zupa = bs(html, "html.parser")
+
+    galvena = zupa.find("div", class_="product_pod")
+    print(galvena)
+    #tabulas = galvena.find_all("article")
+
+    # for tabula in galvena:
+    #     print(tabula)
+    #     print("=======================")
+    #     print("=======================")
+    #     print("=======================")
+
+    #auto_tabula = tabulas[1]
+
+    # rindas = auto_tabula.find_all("tr")
+
+    # for rinda in rindas[1:-1]:
+    #     auto = {}
+    #     # print(rinda)
+    #     # print("=======================")
+    #     # print("=======================")
+    #     # print("=======================")    
+
+info("lapas/1_lapa.html")
+
+#lejupieladet_lapas(5)
