93528df8a4d08fc615099079e4126d4a01312e21
reinis dreska <rdreska@gmail.com>
2021-10-07 13:01:46 +0300

    vertejusm V2

--- scraper.py
+++ scraper.py
@@ -46,12 +46,8 @@ def info(datne):
         print(gramata['nosaukums'])
         gramata['vertejums'] = row.find('p')['class']
         gramata['vertejums'] =str(gramata['vertejums'])
-        start = 0
-        stop = 16
-        if len(gramata['vertejums']) > stop :
-            gramata['vertejums'] = gramata['vertejums'][0: start:] + gramata['vertejums'][stop + 1::]
-            size = len(gramata['vertejums'])
-            gramata['vertejums'] = gramata['vertejums'][:size - 2]
+        size = len(gramata['vertejums'])
+        gramata['vertejums'] = gramata['vertejums'][17:size - 2]
         print(gramata['vertejums'])
 
 info("lapas/1_lapa.html")
